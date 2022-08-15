ca3943a1ee528484785f5d8b9a0e4d57aeb88530
Jan Ole Bangen <jan.bangen@uib.no>
2022-08-15 15:12:51 +0200

    add basic layouts

---
+++ layouts/default.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <NavBar />
+    <div class="container-md p-4 min-vh-100" style="background-color: white">
+      <SearchBar />
+      <slot />
+    </div>
+    <Footer />
+  </div>
+</template>
+
---
+++ layouts/titlepage.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <NavBar />
+    <div class="container-md p-4 min-vh-100" style="background-color: white">
+      <h1 class="py-3 px-1">Termportalen</h1>
+      <SearchBar />
+      <slot />
+    </div>
+    <Footer />
+  </div>
+</template>
+
+<style>
+</style>
