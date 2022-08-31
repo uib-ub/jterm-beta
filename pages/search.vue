554849bb60a0630ac0c51d5e5db03118382846c5
Jan Ole Bangen <jan.bangen@uib.no>
2022-08-31 14:58:02 +0200

    bump search page, api, diaplay of matches

--- pages/search.vue
+++ pages/search.vue
@@ -1,42 +1,38 @@
 <template>
-    <div class="p-1">
-        <ul>
-            <li v-for="item in data">
-                <NuxtLink :to="`/${item.link}`">{{ item.label }}, {{ item.lang }} {{ item.link }}</NuxtLink>
-            </li>
-        </ul>
-    </div>
+  <div class="list-group">
+    <NuxtLink
+      class="list-group-item list-group-item-action"
+      v-for="item in data"
+      :to="`/${item.link}`"
+    >
+      <div>{{ item.label }}</div>
+      <div>({{ localizationData[item.lang] }})</div>
+    </NuxtLink>
+  </div>
 </template>
 
 <script setup>
-definePageMeta({
-    layout: "titlepage",
-});
-
-const data = ref({})
+const data = ref({});
 const route = useRoute();
-const searchterm = useSearchterm()
-const searchLanguage = useSearchLanguage()
+const searchterm = useSearchterm();
+const searchLanguage = useSearchLanguage();
 
 async function fetchData() {
-    data.value = await $fetch('/api/prod', {
-        method: 'post',
-        body: generateSearchQuery(searchterm.value),
-    })
-    data.value = data.value.results.bindings.map(processBindings);
+  data.value = await $fetch("/api/termjson", {
+    method: "post",
+    body: generateSearchQuery(searchterm.value),
+  });
+  data.value = data.value.results.bindings.map(processBindings);
 }
-fetchData()
-watch(route, fetchData)
-
-
+fetchData();
+watch(route, fetchData);
 
 function processBindings(binding) {
-    let link = binding.art.value.split("/").at(-1).replace("-3A", "/");
-    return {
-        label: binding.tword.value,
-        link: link,
-        lang: binding.tword["xml:lang"],
-    };
+  const link = binding.art.value.split("/").at(-1).replace("-3A", "/");
+  return {
+    label: binding.tword.value,
+    link: link,
+    lang: binding.tword["xml:lang"],
+  };
 }
-
 </script>
