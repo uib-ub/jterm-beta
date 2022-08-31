bdc8773a914640d59de72bc7bc457f76db3c11ff
Jan Ole Bangen <jan.bangen@uib.no>
2022-08-31 14:56:00 +0200

    refactor generation of queries

--- composables/generateConceptQuery.ts
+++ composables/generateConceptQuery.ts
@@ -1,19 +1,52 @@
+import sparqljs from 'sparqljs';
+
 export function generateConceptQuery(samling: string, begrep: string) {
+  const { Parser, Generator } = sparqljs;
+  const parser = new Parser();
+  const generator = new Generator();
 
   const query =
     `
+    PREFIX dc: <http://purl.org/dc/elements/1.1/>
+    PREFIX dcterms: <http://purl.org/dc/terms/>
+    PREFIX xkos: <http://rdf-vocabulary.ddialliance.org/xkos#>
+    PREFIX text: <http://jena.apache.org/text#>
+    PREFIX skosxl: <http://www.w3.org/2008/05/skos-xl#>
     PREFIX skos: <http://www.w3.org/2004/02/skos/core#>
-    SELECT ?p0 ?s0 ?p1 ?s1
-    WHERE { GRAPH ?GRAPH {
-    <http://wiki.terminologi.no/index.php/Special:URIResolver/${samling}-3A${begrep}> ?p0 ?s0.
-    NOT EXISTS {?s0 a skos:Concept}.
-    NOT EXISTS {?s0 a skos:Collection}.
-    OPTIONAL {?s0 ?p1 ?s1. }
-    }}`;
+    PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+    PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+    PREFIX skosp: <http://www.data.ub.uib.no/ns/spraksamlingene/skos#>
+    PREFIX skosno: <https://data.norge.no/vocabulary/skosno#>
+    PREFIX base: <http://wiki.terminologi.no/index.php/Special:URIResolver/>
+    CONSTRUCT  {
+      base:${samling}-3A${begrep} ?p ?o.
+      ?o ?p2 ?o2.
+      ?o2 ?p3 ?o3.}
+    WHERE {
+      GRAPH ?GRAPH {
+        {
+          base:${samling}-3A${begrep} ?p ?o.
+          OPTIONAL {?o ?p2 ?o2.
+            OPTIONAL {?o2 ?p3 ?o3
+              FILTER NOT EXISTS {
+                ?o3 a skos:Concept
+              }
+              FILTER NOT EXISTS {
+                ?o3 a skos:Collection
+              }
+            }
+            FILTER NOT EXISTS {
+              ?o a skos:Collection .
+            }
+            FILTER NOT EXISTS {
+              ?o2 a skos:Collection
+            }
+          }
+         }
+      }}
+    `;
 
-
-  console.log("Newquery: " + query)
-
-
+  //const parsedQuery = parser.parse(construct_query);
+  //const generatedQuery = generator.stringify(parsedQuery);
   return query
 }
--- composables/generateSearchQuery.ts
+++ composables/generateSearchQuery.ts
@@ -4,33 +4,34 @@ export function generateSearchQuery(searchterm: string) {
   const searchLanguage = useSearchLanguage()
 
   const { Parser, Generator } = sparqljs;
-  const parser = new Parser();
+  const parser = new Parser({
+    prefixes: {
+      skosxl: 'http://www.w3.org/2008/05/skos-xl#',
+      skosp: 'http://www.data.ub.uib.no/ns/spraksamlingene/skos#',
+      text: 'http://jena.apache.org/text#'
+    }
+  });
   const generator = new Generator();
 
 
   const query = `
-    PREFIX skosxl: <http://www.w3.org/2008/05/skos-xl#>
-    PREFIX skosp: <http://www.data.ub.uib.no/ns/spraksamlingene/skos#>
-    PREFIX text: <http://jena.apache.org/text#>
     SELECT distinct ?art ?tword ?samling
-    WHERE
-    {
-      GRAPH <urn:x-arq:UnionGraph>
-      {
+    WHERE {
+      GRAPH <urn:x-arq:UnionGraph> {
         {
           SELECT  ?lab ?tword
-          WHERE  { ?lab text:query "".}
+          WHERE  { ?lab text:query "". }
           ORDER BY str(?tword) LIMIT 100
         }
-        ?art ?p ?lab.
-        ?art skosp:memberOf ?samling.
+        ?art ?p ?lab;
+          skosp:memberOf ?samling.
         ?lab skosxl:literalForm ?tword.
       }
     }`;
-    
-    const parsedQuery = parser.parse(query);
-    parsedQuery.where[0].patterns[0].patterns[0].where[0].triples[0].object.value = searchterm + "*"
-    parsedQuery.where[0].patterns[0].patterns[0].where[0].triples[0].object.language = searchLanguage.value
-    const generatedQuery = generator.stringify(parsedQuery);
-    return generatedQuery
+
+  const parsedQuery = parser.parse(query);
+  parsedQuery.where[0].patterns[0].patterns[0].where[0].triples[0].object.value = searchterm + "*"
+  parsedQuery.where[0].patterns[0].patterns[0].where[0].triples[0].object.language = searchLanguage.value
+  const generatedQuery = generator.stringify(parsedQuery);
+  return generatedQuery
 }
