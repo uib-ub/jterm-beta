4ee933513c45b1bfe656b591aa1c95958414c475
Jan Ole Bangen <jan.bangen@uib.no>
2022-08-31 14:58:43 +0200

    fix data display languages in page config

--- pages/config.vue
+++ pages/config.vue
@@ -1,20 +1,26 @@
 <template>
-  <div class="container">
-    <h3>Config</h3>
+  <div>
 
-    <h4>Data display languages</h4>
-    <div class="form-check">
-      <input class="form-check-input" type="checkbox" id="ddl-nb" value="nb" v-model="dataDisplayLanguages">
-      <label for="nb">bokmål</label><br>
-      <input class="form-check-input" type="checkbox" id="ddl-nn" value="nn" v-model="dataDisplayLanguages">
-      <label for="nn">nynorsk</label><br>
-      <input class="form-check-input" type="checkbox" id="ddl-en" value="en" v-model="dataDisplayLanguages">
-      <label for="en">engelsk</label><br>
-      <input class="form-check-input" type="checkbox" id="ddl-la" value="la" v-model="dataDisplayLanguages">
-      <label for="la">latin</label><br>
+    <Head>
+      <Title>Termportalen config</Title>
+    </Head>
+    <div class="container">
+      <h1>Config</h1>
+
+      <h2>Data display languages</h2>
+      <div class="form-check">
+        <input class="form-check-input" type="checkbox" id="ddl-nb" value="nb" v-model="dataDisplayLanguages">
+        <label for="ddl-nb">bokmål</label><br>
+        <input class="form-check-input" type="checkbox" id="ddl-nn" value="nn" v-model="dataDisplayLanguages">
+        <label for="ddl-nn">nynorsk</label><br>
+        <input class="form-check-input" type="checkbox" id="ddl-en" value="en" v-model="dataDisplayLanguages">
+        <label for="ddl-en">engelsk</label><br>
+        <input class="form-check-input" type="checkbox" id="ddl-la" value="la" v-model="dataDisplayLanguages">
+        <label for="ddl-la">latin</label><br>
+      </div>
     </div>
-  </div>
 
+  </div>
 </template>
 
 <script setup>
