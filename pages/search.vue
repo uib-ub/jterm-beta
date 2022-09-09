a400cac761b62c0429c039af8d1c3e911c284c77
Jan Ole Bangen <jan.bangen@uib.no>
2022-09-09 14:52:17 +0200

    refactor and expand srarch view (test)

--- pages/search.vue
+++ pages/search.vue
@@ -1,30 +1,46 @@
 <template>
   <div>
-    <div class="container p-4">{{ data.length }} resultater</div>
+    <SearchFilter />
     <div class="list-group">
       <NuxtLink
         class="list-group-item list-group-item-action"
-        v-for="item in data"
+        v-for="item in searchData"
         :to="`/${item.link}`"
       >
-        <div>{{ item.label }}</div>
-        <div>({{ localizationData[item.lang] }})</div>
+        <div class="d-flex flex-lg-row">
+          <div class="container p-0">
+            <div v-if="item.predicate == 'prefLabel'">
+              <div class="container p-0">
+                <b>{{ item.label }}</b>
+              </div>
+            </div>
+            <div v-else>
+              <div class="container p-0">{{ item.label }}</div>
+            </div>
+          </div>
+
+          <div class="container px-3">
+            {{ localizationData[item.lang] || item.lang }}
+          </div>
+          <div class="container p-0">{{ item.samling }}</div>
+        </div>
       </NuxtLink>
+      <div>{{ searchData }}</div>
     </div>
   </div>
 </template>
 
 <script setup>
-const data = ref({});
 const route = useRoute();
+const searchData = useSearchData();
 const searchterm = useSearchterm();
 
 async function fetchData() {
-  data.value = await $fetch("/api/termjson", {
+  searchData.value = await $fetch("/api/termjson", {
     method: "post",
     body: generateSearchQuery(searchterm.value),
   });
-  data.value = data.value.results.bindings.map(processBindings);
+  searchData.value = searchData.value.results.bindings.map(processBindings);
 }
 fetchData();
 watch(route, fetchData);
