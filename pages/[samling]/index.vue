cc4cb3efbd0e5992cdfe05fbb7f6b194058bdc57
Jan Ole Bangen <jan.bangen@uib.no>
2022-09-16 13:50:32 +0200

    updd samling template

--- pages/[samling]/index.vue
+++ pages/[samling]/index.vue
@@ -1,8 +1,99 @@
 <template>
   <div>
-    <ContentDoc>
-      <template #not-found> {{ $t("samling.not-found") }} </template>
-    </ContentDoc>
+    <h1 class="py-3">
+      {{ displayData[uri]?.["rdfs:label"][0]["@value"] }}
+    </h1>
+    <div class="container p-0">
+      <div class="row row-cols-1 row-cols-md-2">
+        <!--Description-->
+        <div class="col">
+          <p v-for="p in descriptionData">
+            {{ p }}
+          </p>
+        </div>
+        <!--Table-->
+        <div class="col">
+          <table class="table table-sm table-hover table-borderless">
+            <tbody>
+              <!--Organisation-->
+              <tr
+                v-if="displayData?.[`${uri}-23org`]?.['rdfs:label']?.['@value']"
+              >
+                <th class="col-3" scope="row">
+                  {{ $t("samling.organisation") }}
+                </th>
+                <td>
+                  {{ displayData[`${uri}-23org`]["rdfs:label"]["@value"] }}
+                </td>
+              </tr>
+              <!--Organisation number-->
+              <tr v-if="displayData?.[`${uri}-23org`]?.['dcterms:identifier']">
+                <th class="col" scope="row">
+                  {{ $t("samling.orgnr") }}
+                </th>
+                <td>
+                  {{ displayData[`${uri}-23org`]["dcterms:identifier"] }}
+                </td>
+              </tr>
+              <!--Email-->
+              <tr v-if="displayData?.[`${uri}-23contact`]?.['vcard:hasEmail']">
+                <th class="col" scope="row">
+                  {{ $t("samling.email") }}
+                </th>
+                <td>
+                  <a
+                    :href="displayData[`${uri}-23contact`]?.['vcard:hasEmail']['@id']"
+                    >{{
+                      displayData[`${uri}-23contact`]?.["vcard:hasEmail"][
+                        "@id"
+                      ].split(":")[1]
+                    }}</a
+                  >
+                </td>
+              </tr>
+              <!--Telephone-->
+              <tr
+                v-if="displayData?.[`${uri}-23contact`]?.['vcard:hasTelephone']"
+              >
+                <th class="col" scope="row">
+                  {{ $t("samling.telephone") }}
+                </th>
+                <td>
+                  {{ displayData[`${uri}-23contact`]?.["vcard:hasTelephone"] }}
+                </td>
+              </tr>
+              <!--Languages-->
+              <tr v-if="displayData[uri]?.['dcterms:language']">
+                <th class="col" scope="row">
+                  {{ $t("global.language", 1) }}
+                </th>
+                <td>
+                  {{
+                    displayData[uri]["dcterms:language"]
+                      .map((lang: string) => $t("global.lang." + lang))
+                      .join(", ")
+                  }}
+                </td>
+              </tr>
+              <!--Starting languages-->
+              <tr v-if="displayData[uri]?.['skosp:opprinneligSpraak']">
+                <th class="col" scope="row">
+                  {{ $t("samling.startLang") }}
+                </th>
+                <td>
+                  {{
+                    $t(
+                      "global.lang." +
+                        displayData[uri]?.["skosp:opprinneligSpraak"]
+                    )
+                  }}
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
