14f611142805b33b7ffec75ea7dcc7d5f249becf
Jan Ole Bangen <jan.bangen@uib.no>
2022-09-16 16:03:32 +0200

    add component for data row

---
+++ components/DataRow.vue
@@ -0,0 +1,24 @@
+<template>
+  <tr>
+    <th class="class" scope="row">
+      {{ $t(label) }}
+    </th>
+    <td v-if="to">
+      <NuxtLink v-if="nuxtlink" to="to">{{ data || to }}</NuxtLink>
+      <a v-else href="to">{{ data || to }}</a>
+    </td>
+    <td v-else>
+      {{ data }}
+    </td>
+  </tr>
+</template>
+
+<script setup lang="ts">
+const props = defineProps({
+  data: { type: String },
+  class: { type: String, default: "col" },
+  label: { type: String, required: true },
+  to: String,
+  nuxtlink: Boolean,
+});
+</script>
