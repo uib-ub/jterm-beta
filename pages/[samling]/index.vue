c5f0defcdd924c395c61954e11b7f4b53854a8f7
Jan Ole Bangen <jan.bangen@uib.no>
2022-09-20 13:28:45 +0200

    update samling template to reflect compacted data

--- pages/[samling]/index.vue
+++ pages/[samling]/index.vue
@@ -1,17 +1,13 @@
 <template>
   <div>
     <h1 class="py-3">
-      {{ displayData[uri]?.["rdfs:label"][0]["@value"] }}
+      {{ uriData?.label[0]["@value"] }}
     </h1>
     <div class="container p-0">
       <div class="row row-cols-1 row-cols-md-2">
         <!--Description-->
         <div class="col">
-          <p
-            v-for="p in uriData?.['dcterms:description']?.['@value'].split(
-              '\n\n'
-            )"
-          >
+          <p v-for="p in uriData?.description?.['@value'].split('\n\n')">
             {{ p }}
           </p>
         </div>
@@ -21,45 +17,45 @@
             <tbody>
               <!--Organisation-->
               <DataRow
-                v-if="orgData?.['rdfs:label']?.['@value']"
-                :data="orgData['rdfs:label']['@value']"
+                v-if="orgData?.label?.['@value']"
+                :data="orgData.label['@value']"
                 thClass="col-3"
                 :label="$t('samling.organisation')"
               />
               <!--Organisation number-->
               <DataRow
-                v-if="orgData?.['dcterms:identifier']"
-                :data="orgData['dcterms:identifier']"
+                v-if="orgData?.identifier"
+                :data="orgData.identifier"
                 thClass="col-3"
                 :label="$t('samling.orgnr')"
               />
 
               <!--Email-->
               <DataRow
-                v-if="contactData?.['vcard:hasEmail']?.['@id']"
-                :data="contactData['vcard:hasEmail']['@id'].split(':')[1]"
+                v-if="contactData?.hasEmail"
+                :data="contactData.hasEmail.split(':')[1]"
                 thClass="col-3"
                 :label="$t('samling.email')"
-                :to="contactData['vcard:hasEmail']['@id']"
+                :to="contactData.hasEmail"
               />
               <!--Telephone-->
               <DataRow
-                v-if="contactData?.['vcard:hasTelephone']"
-                :data="contactData?.['vcard:hasTelephone']"
+                v-if="contactData?.hasTelephone"
+                :data="contactData?.hasTelephone"
                 thClass="col-3"
                 :label="$t('samling.telephone')"
               />
               <!--Languages-->
               <DataRow
-                v-if="uriData?.['dcterms:language']"
-                :data="uriData['dcterms:language'].map((lang: string) => $t('global.lang.' + lang)) .join(', ')"
+                v-if="uriData?.language"
+                :data="uriData.language.map((lang: string) => $t('global.lang.' + lang)) .join(', ')"
                 thClass="col-3"
                 :label="$t('global.language', 1)"
               />
               <!--Starting languages-->
               <DataRow
-                v-if="uriData?.['skosp:opprinneligSpraak']"
-                :data="$t('global.lang.' + uriData['skosp:opprinneligSpraak'])"
+                v-if="uriData?.opprinneligSpraak"
+                :data="$t('global.lang.' + uriData.opprinneligSpraak)"
                 thClass="col-3"
                 :label="$t('samling.startLang')"
               />
@@ -74,13 +70,12 @@
 <script setup lang="ts">
 const route = useRoute();
 const samling = getSamlingFromParam();
-const uri = `base:${samling}-3A${samling}`;
+const uri = `${samling}-3A${samling}`;
 const samlingData = ref();
 const displayData = computed(() => {
-  return identifyData(samlingData.value?.["@graph"]);
-});
+  return identifyData(samlingData.value?.["@graph"]);});
 const uriData = computed(() => {
-  return displayData.value[uri];
+  return displayData.value?.[uri];
 });
 const orgData = computed(() => {
   return displayData.value?.[`${uri}-23org`];
@@ -103,7 +98,7 @@ async function fetchData() {
     method: "post",
     body: generateSamlingQuery(samling),
   });
-  samlingData.value = data;
+  samlingData.value = await compactData(data);
 }
 
 fetchData();
