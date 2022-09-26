83b20c43ce6c130e8915ba2c9905621b5de99a15
Jan Ole Bangen <jan.bangen@uib.no>
2022-09-26 11:05:35 +0200

    add component for FilterCheckbox
    require props in FilterCheckBox

---
+++ components/FilterCheckbox.vue
@@ -0,0 +1,22 @@
+<template>
+  <input
+    class="form-check-input"
+    type="checkbox"
+    id="`filter-${ftype}-${fvalue}`"
+    :value="fvalue"
+    v-model="searchFilterData[ftype]"
+  />
+  <label for="`filter-${ftype}-${fvalue}`"
+    >{{ $t("global." + ftype + "." + fvalue) }} ({{
+      searchDataStats?.[ftype]?.[fvalue]
+    }})</label
+  ><br />
+</template>
+<script setup lang="ts">
+const searchFilterData = useSearchFilterData();
+const searchDataStats = useSearchDataStats();
+const props = defineProps({
+  ftype: { type: String, required: true },
+  fvalue: {type: String, required: true},
+});
+</script>
