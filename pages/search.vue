5165d7e86878d7c72e1ed7435b63c2ddcf4eb0c5
Jan Ole Bangen <jan.bangen@uib.no>
2022-10-14 14:56:14 +0200

    moce preprocessBindings to composable

--- pages/search.vue
+++ pages/search.vue
@@ -45,19 +45,4 @@ async function fetchData() {
 fetchData();
 watch(route, fetchData);
 
-function processBindings(binding) {
-  const link = binding.uri.value.split("/").at(-1).replace("-3A", "/");
-  const samling = binding.samling.value.split("/").at(-1).split("3A").at(-1);
-  const predicate = binding.predicate.value.replace(
-    "http://www.w3.org/2008/05/skos-xl#",
-    ""
-  );
-  return {
-    predicate: predicate,
-    label: binding.term.value,
-    link: link,
-    lang: binding.term["xml:lang"],
-    samling: samling,
-  };
-}
 </script>
