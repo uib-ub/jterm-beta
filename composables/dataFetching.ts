5f7229b85e5de9cfa2f782282890448348198396
Jan Ole Bangen <jan.bangen@uib.no>
2022-10-25 14:29:49 +0200

    add option to pass accept header to fetchData

--- composables/dataFetching.ts
+++ composables/dataFetching.ts
@@ -11,6 +11,8 @@ export async function fetchData(query: string, accept?: string) {
     headers: {
       "Content-type": "application/sparql-query",
       Referer: "https://term.uib.no/",
+      Accept: accept || "application/json",
+
     },
   });
 }
