293542ded19af205e8ee6f0ec18b0abf6d610550
Jan Ole Bangen <jan.bangen@uib.no>
2022-11-17 16:02:50 +0100

    move display of searchresult to own component

---
+++ components/SearchResultEntry.vue
@@ -0,0 +1,45 @@
+<template>
+  <NuxtLink
+    class="list-group-item list-group-item-action"
+    :to="`/${entryData.link}`"
+  >
+    <div class="d-flex flex-lg-row">
+      <div class="container p-0">
+        <div v-if="entryData.predicate == 'prefLabel'">
+          <div class="container p-0">
+            <b v-html="entryData.label"></b>
+          </div>
+        </div>
+        <div v-else>
+          <div class="container p-0" v-html="entryData.label"></div>
+        </div>
+      </div>
+      <div class="container px-3">
+        {{
+          entryData.lang.map((l: string) => $t(`global.lang.${l}`)).join(", ")
+        }}
+      </div>
+      <div class="container p-0">{{ entryData.samling }}</div>
+    </div>
+  </NuxtLink>
+</template>
+
+<script setup lang="ts">
+interface Props {
+  entryData: {
+    link: string;
+    predicate: string;
+    label: string;
+    lang: string[];
+    samling: string;
+  };
+}
+
+const props = defineProps<Props>();
+</script>
+
+<style>
+.searchHighlight {
+  background-color: var(--tp-blue-1);
+}
+</style>
--- pages/search.vue
+++ pages/search.vue
@@ -5,31 +5,10 @@
     </Head>
     <SearchFilter />
     <div class="list-group">
-      <NuxtLink
-        class="list-group-item list-group-item-action"
-        v-for="item in searchDataFiltered.slice(0, 50)"
-        :to="`/${item.link}`"
-      >
-        <div class="d-flex flex-lg-row">
-          <div class="container p-0">
-            <div v-if="item.predicate == 'prefLabel'">
-              <div class="container p-0">
-                <b v-html="item.label"></b>
-              </div>
-            </div>
-            <div v-else>
-              <div class="container p-0" v-html="item.label"></div>
-            </div>
-          </div>
-
-          <div class="container px-3">
-            {{
-              item.lang.map((l: string) => $t(`global.lang.${l}`)).join(", ")
-            }}
-          </div>
-          <div class="container p-0">{{ item.samling }}</div>
-        </div>
-      </NuxtLink>
+      <SearchResultEntry
+        v-for="entry in searchDataFiltered"
+        :entryData="entry"
+      />
     </div>
   </div>
 </template>
@@ -37,9 +16,3 @@
 <script setup lang="ts">
 const searchDataFiltered = useSearchDataFiltered();
 </script>
-
-<style>
-.searchHighlight {
-  background-color: var(--tp-blue-1);
-}
-</style>
