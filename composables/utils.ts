235a2cda1dedc9ce1a43f1feb4bc2a288dd3bbde
Jan Ole Bangen <jan.bangen@uib.no>
2022-11-18 14:58:58 +0100

    add utils sum()

--- composables/utils.ts
+++ composables/utils.ts
@@ -4,3 +4,7 @@ export function intersectUnique(a: any[], b: any[]) {
   var intersection = new Set([...setA].filter((x) => setB.has(x)));
   return Array.from(intersection);
 }
+
+export function sum(numbers: number[]) {
+  return numbers.reduce((a, b) => a + b, 0);
+}
