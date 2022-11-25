6582a7f8888b6216f73a8b4f90eff5b19ff0bd4b
Jan Ole Bangen <jan.bangen@uib.no>
2022-11-25 18:17:15 +0100

    refactor and rename fetchSearchData

---
+++ composables/useFetchSearchData.ts
@@ -0,0 +1,119 @@
+import { SearchDataEntry, SearchDataStats, SearchOptions } from "./states";
+
+export async function fetchSearchDataMatching(
+  searchOptions: SearchOptions,
+  matching: string[],
+  append: boolean,
+  currentFetch: number
+) {
+  const searchData = useSearchData();
+  const searchFetchLatest = useSearchFetchLatest();
+  const data = await fetchData(
+    genSearchQuery(searchOptions, "entries", matching)
+  );
+  if (currentFetch == searchFetchLatest.value) {
+    if (append) {
+      searchData.value = searchData.value.concat(
+        data.results.bindings.map(processBinding)
+      );
+    } else {
+      searchData.value = data.results.bindings.map(processBinding);
+    }
+  }
+}
+
+async function fetchSearchDataAggregate(
+  searchOptions: SearchOptions,
+  matching: string[],
+  type: string,
+  currentFetch: number
+) {
+  const searchDataStats = useSearchDataStats();
+  const searchFetchLatest = useSearchFetchLatest();
+  const searchDataPending = useSearchDataPending();
+  const data = await fetchData(
+    genSearchQuery(searchOptions, "aggregate", matching)
+  );
+  const newStats = data.results?.bindings.reduce(
+    (o, key) => sumAggregateData(o, key),
+    {}
+  );
+  if (currentFetch === searchFetchLatest.value) {
+    if (type === "initial") {
+      searchDataStats.value = newStats;
+    } else if (type === "filter") {
+      const zeroedStats = resetStats(searchDataStats.value, false);
+      for (const category of Object.keys(zeroedStats)) {
+        searchDataStats.value[category as keyof SearchDataStats] = {
+          ...zeroedStats[category as keyof SearchDataStats],
+          ...newStats[category],
+        };
+      }
+    }
+    searchDataPending.value["aggregate"] = false;
+  }
+}
+
+type FetchTypes = "initial" | "filter" | "further";
+
+export async function useFetchSearchData(
+  searchOptions: SearchOptions,
+  fetchType: FetchTypes,
+  matching?: string[]
+) {
+  const searchData = useSearchData();
+  const searchFetchLatest = useSearchFetchLatest();
+  const searchDataPending = useSearchDataPending();
+  const searchFilterData = useSearchFilterData();
+  let append = false;
+  const fetchTime = Date.now();
+  searchFetchLatest.value = fetchTime;
+
+  let searchMatching = matching || ["all"];
+  if (!matching || searchOptions.searchTerm.length > 0) {
+    searchMatching = searchOptions.searchMatching;
+  }
+
+  if (fetchType == "initial") {
+    searchFilterData.value = {
+      lang: [],
+      samling: [],
+      predicate: [],
+      matching: [],
+    };
+  }
+
+  if (fetchType === "initial" || fetchType === "filter") {
+    searchDataPending.value["aggregate"] = true;
+    fetchSearchDataAggregate(
+      searchOptions,
+      searchMatching.flat(),
+      fetchType,
+      fetchTime
+    );
+  }
+
+  if (fetchType === "further") {
+    append = true;
+  }
+
+  searchDataPending.value["entries"] = true;
+  for (const m of searchMatching) {
+    await fetchSearchDataMatching(
+      searchOptions,
+      Array.isArray(m) ? m : [m],
+      append,
+      fetchTime
+    );
+    append = true;
+    if (fetchTime != searchFetchLatest.value) {
+      break;
+    }
+    if (searchData.value.length >= searchOptions.searchLimit) {
+      break;
+    }
+  }
+  if (fetchTime == searchFetchLatest.value) {
+    searchDataPending.value["entries"] = false;
+  }
+}
