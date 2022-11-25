30e6a61a233214ff1970d03c521e894778399da8
Jan Ole Bangen <jan.bangen@uib.no>
2022-11-25 18:10:35 +0100

    add function to calc real count of searchEntries

--- utils/utils.ts
+++ utils/utils.ts
@@ -1,3 +1,5 @@
+import { SearchDataEntry } from "~~/composables/states";
+
 export function intersectUnique(a: any[], b: any[]) {
   var setA = new Set(a);
   var setB = new Set(b);
@@ -7,4 +9,17 @@ export function intersectUnique(a: any[], b: any[]) {
 
 export function sum(numbers: number[]) {
   return numbers.reduce((a, b) => a + b, 0);
+}
+
+/**
+ * Returns real count of searchentries based on the length of their language properties.
+ *
+ * @param matches list of searchentries that contain languages as a list: lang: {"nb", "nny"}
+ */
+export function countSearchEntries(matches: SearchDataEntry[]): number {
+  return sum(
+    matches.map((entry) => {
+      return entry.lang.length;
+    })
+  );
 }
