35945a3c5624367571d49168f341b7eff836f4dd
Jan Ole Bangen <jan.bangen@uib.no>
2022-11-25 18:19:11 +0100

    upd search page with refactored fetch/pending/timeouts

--- pages/search.vue
+++ pages/search.vue
@@ -6,13 +6,13 @@
     <SearchFilter />
     <div class="list-group" ref="scrollComponent">
       <SearchResultEntry
-        v-for="entry in searchDataFiltered"
+        v-for="entry in searchData"
         :entryData="entry"
       />
     </div>
     <div class="d-flex justify-content-center p-2">
       <div
-        v-if="fetchFurtherPending"
+        v-if="pending && countFetchedMatches > 30"
         class="spinner-border"
         style="width: 1.75rem; height: 1.75rem"
         role="status"
@@ -24,34 +24,27 @@
 </template>
 
 <script setup lang="ts">
-const searchDataFiltered = useSearchDataFiltered();
+const searchData = useSearchData();
 const searchFilterData = useSearchFilterData();
 const searchDataStats = useSearchDataStats();
-const countMatches = computed(() => {
-  const langNumbers = searchDataFiltered.value.map((entry) => {
-    return entry.lang.length;
-  });
-  return sum(langNumbers);
+const countFetchedMatches = computed(() => {
+  return countSearchEntries(searchData.value);
 });
 const searchOptions = useSearchOptions();
-const searchDataCount = useSearchDataCount();
-const countValues = computed(() => {
-  return (
-    searchDataCount.value?.results.bindings.map((binding) => {
-      if (binding.count) {
-        return parseInt(binding.count.value);
-      } else {
-        return 0;
-      }
-    }) || [0]
-  );
-});
 const count = computed(() => {
-  return countValues.value.reduce((partial, current) => partial + current, 0);
+  try {
+    return sum(Object.values(searchDataStats.value?.["matching"])) || 0;
+  } catch (e) {
+    return 0;
+  }
+});
+
+const searchDataPending = useSearchDataPending();
+const pending = computed(() => {
+  return !Object.values(searchDataPending.value).every((el) => !el)
 });
 
 const scrollComponent = ref(null);
-const fetchFurtherPending = ref(false);
 
 onMounted(() => {
   window.addEventListener("scroll", fetchFurtherSearchData);
@@ -61,16 +54,16 @@ onUnmounted(() => {
 });
 
 const fetchFurtherSearchData = () => {
+  const searchFetchLatest = useSearchFetchLatest();
   let element = scrollComponent.value;
-  if (count.value > countMatches.value && !fetchFurtherPending.value) {
+  if (count.value > countFetchedMatches.value && !pending.value) {
     if (element.getBoundingClientRect().bottom * 0.75 < window.innerHeight) {
-      fetchFurtherPending.value = true;
 
       let newOffsetCalc;
-      let oldOffsetCalc = countMatches.value;
+      let oldOffsetCalc = countFetchedMatches.value;
       let fetchNextMatching = false;
       const offset = {};
-      for (const match of searchOptions.value.searchMatching) {
+      for (const match of searchOptions.value.searchMatching.flat()) {
         if (Object.keys(searchDataStats.value.matching).includes(match)) {
           const matchCount = searchDataStats.value.matching[match];
           if (fetchNextMatching) {
@@ -108,15 +101,26 @@ const fetchFurtherSearchData = () => {
         searchLimit: searchOptions.value.searchLimit,
         searchOffset: offset,
       };
+      const fetchTime = Date.now();
+      searchFetchLatest.value = fetchTime;
+
+      fetchSearchData(
+        newOptions,
+        "further",
+        Object.keys(offset)
+      );
+      /*
       fetchSearchDataMatching(
         newOptions,
         Object.keys(offset),
         searchDataFiltered,
-        true
+        true,
+        fetchTime
       );
       setTimeout(function () {
         fetchFurtherPending.value = false;
       }, 500);
+      */
     }
   }
 };
