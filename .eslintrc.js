c87805006b6f021a7b2149268b4a665f5cc7cc74
Jan Ole Bangen <jan.bangen@uib.no>
2022-11-29 15:09:32 +0100

    add eslinting for tsdoc

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,8 @@
 module.exports = {
+  plugins: ["eslint-plugin-tsdoc"],
   extends: ["@nuxtjs/eslint-config-typescript", "prettier"],
   rules: {
     "@typescript-eslint/no-unused-vars": ["off"],
+    "tsdoc/syntax": "warn",
   },
 };
