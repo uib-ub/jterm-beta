a113cbbaf936ccdc3ca29bfcef3db4023d603315
Jan Ole Bangen <jan.bangen@uib.no>
2022-12-06 13:28:00 +0100

    type fetchData

--- utils/dataFetching.ts
+++ utils/dataFetching.ts
@@ -1,12 +1,17 @@
-export async function fetchData(query: string, accept?: string) {
-    const url = "https://sparql.terminologi.no/termwiki_test/query";
-    return await $fetch(url, {
-      method: "post",
-      body: query,
-      headers: {
-        "Content-type": "application/sparql-query",
-        Referer: "https://term.uib.no/",
-        Accept: accept || "application/json",
-      },
-    });
-  }
+import { SearchQueryResponse } from "./vars";
+
+export async function fetchData(
+  query: string,
+  accept?: string
+): Promise<SearchQueryResponse> {
+  const url = "https://sparql.terminologi.no/termwiki_test/query";
+  return await $fetch(url, {
+    method: "post",
+    body: query,
+    headers: {
+      "Content-type": "application/sparql-query",
+      Referer: "https://term.uib.no/",
+      Accept: accept || "application/json",
+    },
+  });
+}
