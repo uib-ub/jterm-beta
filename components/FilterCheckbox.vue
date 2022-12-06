a7672ec0e19d2f8da65e7dac376ae83b1cb358fd
Jan Ole Bangen <jan.bangen@uib.no>
2022-12-06 13:10:10 +0100

    reorder props and typing of FilterCheckbox

--- components/FilterCheckbox.vue
+++ components/FilterCheckbox.vue
@@ -1,19 +1,22 @@
 <template>
   <input
+    id="`filter-${ftype}-${fvalue}`"
+    v-model="searchFilterData[ftype as keyof SearchDataStats]"
     class="form-check-input"
     type="checkbox"
-    id="`filter-${ftype}-${fvalue}`"
     :value="fvalue"
-    v-model="searchFilterData[ftype]"
   />
   <label for="`filter-${ftype}-${fvalue}`"
     >{{ $t("global." + ftype + "." + fvalue) }} ({{
-      searchDataStats[ftype as keyof SearchDataStats][fvalue]
+      searchDataStats[ftype as keyof SearchDataStats][
+        fvalue as LangCode | Samling | Matching | LabelPredicate
+      ]
     }})</label
   ><br />
 </template>
 <script setup lang="ts">
 import { SearchDataStats } from "~~/composables/states";
+import { LangCode, Samling, Matching, LabelPredicate } from "~~/utils/vars";
 
 const searchFilterData = useSearchFilterData();
 const searchDataStats = useSearchDataStats();
