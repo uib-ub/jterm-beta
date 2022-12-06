57647ec29113274480feba8ee207c8fda4fb4eaa
Jan Ole Bangen <jan.bangen@uib.no>
2022-12-06 16:18:07 +0100

    fix id and label creation for filterCheckbox

--- components/FilterCheckbox.vue
+++ components/FilterCheckbox.vue
@@ -1,12 +1,12 @@
 <template>
   <input
-    id="`filter-${ftype}-${fvalue}`"
+    :id="`filter-${ftype}-${fvalue}`"
     v-model="searchFilterData[ftype as keyof SearchDataStats]"
     class="form-check-input"
     type="checkbox"
     :value="fvalue"
   />
-  <label for="`filter-${ftype}-${fvalue}`"
+  <label :for="`filter-${ftype}-${fvalue}`"
     >{{ $t("global." + ftype + "." + fvalue) }} ({{
       searchDataStats[ftype as keyof SearchDataStats][
         fvalue as LangCode | Samling | Matching | LabelPredicate
