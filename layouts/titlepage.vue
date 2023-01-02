db715a40a400dd1732fbb72f3401bf4416cc247a
Jan Ole Bangen <jan.bangen@uib.no>
2023-01-02 16:31:36 +0100

    upd titlepage layout

--- layouts/titlepage.vue
+++ layouts/titlepage.vue
@@ -1,8 +1,15 @@
 <template>
-  <div>
+  <div class="flex h-screen flex-col">
     <NavBar />
-    <main role="main" class="container p-4" style="background-color: white; min-height: calc(100vh - 142px)">
-      <h1 class="display-3 py-3">Termportalen</h1>
+    <main
+      role="main"
+      class="container mx-auto max-w-screen-lg flex-1 bg-white p-2 sm:p-4 md:p-7 lg:p-10"
+    >
+      <h1
+        class="xs:text-6xl py-6 text-5xl font-light tracking-tight text-black/75 antialiased sm:text-7xl md:text-8xl"
+      >
+        Termportalen
+      </h1>
       <SearchBar />
       <slot />
     </main>
