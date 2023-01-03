cdc36c7a386c414456fa0c6a871d22125a8f4dec
Jan Ole Bangen <jan.bangen@uib.no>
2023-01-03 15:50:12 +0100

    use default layout for titlepage
    avoids rerendering/flickering of navbar

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,10 +1,23 @@
 <template>
-  <div class="h-screen flex flex-col">
+  <div class="flex h-screen flex-col">
     <NavBar />
-    <main role=main class="container flex-1 mx-auto max-w-screen-lg p-5 bg-white">
+    <main
+      role="main"
+      class="container mx-auto max-w-screen-lg flex-1 bg-white p-5"
+    >
+      <h1
+        v-if="route.path === '/'"
+        class="xs:text-6xl pb-6 text-5xl font-light tracking-tight text-black/75 antialiased sm:text-7xl md:text-8xl"
+      >
+        Termportalen
+      </h1>
       <SearchBar />
       <slot />
     </main>
     <FooterTP />
   </div>
 </template>
+
+<script setup>
+const route = useRoute();
+</script>
