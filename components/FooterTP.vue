00420da0206f5a052ac0e2859feb3b7ec6f11e07
Jan Ole Bangen <jan.bangen@uib.no>
2023-01-03 15:58:55 +0100

    upd component FooterTP

--- components/FooterTP.vue
+++ components/FooterTP.vue
@@ -1,22 +1,26 @@
 <template>
-  <footer style="background-color: var(--tp-blue-4)" role="contentinfo">
-    <div
-      class="d-flex justify-content-center align-items-center"
-      style="color: var(--tp-light)"
-    >
+  <footer
+    class="bg-tpblue-400 static left-0 right-0 bottom-0 text-white"
+    role="contentinfo"
+  >
+    <div class="flex flex-wrap justify-center">
       <img
         src="/logo_sprakradet_neg.png"
         class="img-fluid"
         style="max-height: 28px"
         alt="logo språkrådet"
       />
-      <img src="/logo_ugle_white.svg" class="img-fluid my-2 mx-4" alt="logo uib" />
-      <p>Kontaktere oss... etc</p>
+      <img
+        src="/logo_ugle_white.svg"
+        class="img-fluid my-2 mx-4"
+        alt="logo uib"
+      />
+      <span>Kontaktere oss... etc</span>
     </div>
   </footer>
 </template>
 
-<style>
+<style scoped>
 img {
   max-height: 100px;
 }
