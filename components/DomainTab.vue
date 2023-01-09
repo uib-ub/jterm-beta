e0d093e24056938277ad0498927b9c3d15128289
Jan Ole Bangen <jan.bangen@uib.no>
2023-01-09 14:24:59 +0100

    add component for domain tab
    fix domainTab with option

---
+++ components/DomainTab.vue
@@ -0,0 +1,29 @@
+<template>
+  <li class="hover:bg-gray-100 shrink-0" role="presentation">
+    <button
+      :id="`${tabId}Domains-tab`"
+      class="rounded-t border border-b-0 border-gray-300 px-2 py-1 active:bg-gray-200"
+      :class="{ activeDomain: searchOptions.searchDomain[0] == tabId }"
+      type="button"
+      role="tab"
+      @click="searchOptions.searchDomain = [tabId]"
+    >
+      {{ $t("global.domain." + tabId) }}
+    </button>
+  </li>
+</template>
+
+<script setup>
+const searchOptions = useSearchOptions();
+
+const props = defineProps({
+  tabId: { type: String, required: true },
+});
+
+</script>
+
+<style>
+.activeDomain {
+  @apply bg-gray-300;
+}
+</style>
