ac788887985cd178aef202538ee425af9f1a2b7e
Jan Ole Bangen <jan.bangen@uib.no>
2023-01-09 14:25:31 +0100

    add component for domaintabs
    fix domainTabs with options

---
+++ components/DomainTabs.vue
@@ -0,0 +1,54 @@
+<template>
+  <div>
+    <div>
+      <ul id="domainTab" class="flex gap-2 overflow-x-auto px-1" role="tablist">
+        <DomainTab tab-id="all" />
+        <DomainTab tab-id="humaniora" />
+        <DomainTab tab-id="naturvitenskap" />
+        <DomainTab tab-id="sammfunnsfag" />
+        <DomainTab tab-id="okonomAdmin" />
+      </ul>
+      <button
+        id="expand-bt"
+        class="hidden"
+        type="button"
+        @click="displayDetails = !displayDetails"
+      >
+        Utvide subdomener
+      </button>
+    </div>
+    <div
+      v-if="displayDetails"
+      id="domainTabDetails"
+      class="rounded border border-solid border-gray-300"
+    >
+      <DomainTabDetails
+        v-if="searchOptions.searchDomain[0] === 'all'"
+        tab-id="all"
+      />
+      <DomainTabDetails
+        v-if="searchOptions.searchDomain[0] === 'humaniora'"
+        tab-id="humaniora"
+      />
+      <DomainTabDetails
+        v-if="searchOptions.searchDomain[0] === 'naturvitenskap'"
+        tab-id="naturvitenskap"
+      />
+      <DomainTabDetails
+        v-if="searchOptions.searchDomain[0] === 'sammfunnsfag'"
+        tab-id="sammfunnsfag"
+      />
+      <DomainTabDetails
+        v-if="searchOptions.searchDomain[0] === 'okonomAdmin'"
+        tab-id="okonomAdmin"
+      />
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+const searchOptions = useSearchOptions();
+const displayDetails = ref(false);
+</script>
+
+<style></style>
