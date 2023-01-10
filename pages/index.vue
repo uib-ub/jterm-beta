e1ec485afa50c125fa2b0bc3c6d7fecb8c4b7821
Jan Ole Bangen <jan.bangen@uib.no>
2023-01-10 19:41:38 +0100

    improve responsivity of landing page by adding space for list view

--- pages/index.vue
+++ pages/index.vue
@@ -4,7 +4,7 @@
       <Title>{{ $t("index.title") }}</Title>
     </Head>
     <WelcomeTP class="md:basis-3/5 md:pr-4"/>
-    <DomainOverview class="md:basis-2/5"/>
+    <DomainOverview class="md:basis-2/5 pt-5 md:pt-0"/>
   </div>
 </template>
 
