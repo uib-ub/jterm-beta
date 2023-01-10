a2cad2976323244f4b579e76fa5a7ad15ce22b70
Jan Ole Bangen <jan.bangen@uib.no>
2023-01-10 15:49:56 +0100

    add component for domain overview
    fix domain overview

---
+++ components/DomainOverview.vue
@@ -0,0 +1,14 @@
+z
+<template>
+  <div>
+    <h2 id="domener" class="text-2xl pb-1">
+      <NuxtLink to="#domener">Domener & Termbaser</NuxtLink>
+    </h2>
+    <ul class="pl-1">
+      <DomainOverviewEntry domain="humaniora"></DomainOverviewEntry>
+      <DomainOverviewEntry domain="naturvitenskap"></DomainOverviewEntry>
+      <DomainOverviewEntry domain="sammfunnsfag"></DomainOverviewEntry>
+      <DomainOverviewEntry domain="okonomAdmin"></DomainOverviewEntry>
+    </ul>
+  </div>
+</template>
