4bd749de970b649ab035bb3d1b979f52413d6d3d
Jan Ole Bangen <jan.bangen@uib.no>
2023-01-12 09:27:42 +0100

    add localization for domain overview

--- components/DomainOverview.vue
+++ components/DomainOverview.vue
@@ -2,7 +2,7 @@ z
 <template>
   <div>
     <h2 id="domener" class="text-2xl pb-1">
-      <NuxtLink to="#domener">Domener & Termbaser</NuxtLink>
+      <NuxtLink to="#domener">{{ $t("domainOverview.domainsAndBases") }}</NuxtLink>
     </h2>
     <ul class="pl-1">
       <DomainOverviewEntry domain="humaniora"></DomainOverviewEntry>
