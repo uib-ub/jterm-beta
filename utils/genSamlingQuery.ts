b639453ef9d7c6c08810f85d6681a40560bc4b9c
Jan Ole Bangen <jan.bangen@uib.no>
2023-01-13 16:28:37 +0100

    use env, base in samling query

--- utils/genSamlingQuery.ts
+++ utils/genSamlingQuery.ts
@@ -1,11 +1,12 @@
 export function genSamlingQuery(namespace: string): string {
+  const runtimeConfig = useRuntimeConfig();
   const query = `
     PREFIX dcterms: <http://purl.org/dc/terms/>
     PREFIX skos: <http://www.w3.org/2004/02/skos/core#>
     PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
     PREFIX skosp: <http://www.data.ub.uib.no/ns/spraksamlingene/skos#>
     PREFIX vcard: <http://www.w3.org/2006/vcard/ns#>
-    PREFIX base: <http://test.wiki.terminologi.no/index.php/Special:URIResolver/>
+    PREFIX base: <${runtimeConfig.public.base}>
     CONSTRUCT  {
       base:${namespace}-3A${namespace} ?p ?o.
       ?o ?p2 ?o2.}
