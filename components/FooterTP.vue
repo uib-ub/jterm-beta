0d0d6c1bf14176703a6d94abe8ebd2a396be3e1e
Jan Ole Bangen <jan.bangen@uib.no>
2023-01-13 11:51:21 +0100

    fix footer sizes/spacing
    image turned out way to large on vercel

--- components/FooterTP.vue
+++ components/FooterTP.vue
@@ -3,13 +3,13 @@
     class="bg-tpblue-400 static left-0 right-0 bottom-0 text-white"
     role="contentinfo"
   >
-    <div class="flex items-center justify-center gap-x-6 p-1 md:py-2">
+    <div class="flex items-center justify-center gap-x-6 p-2">
       <nuxt-img
         src="/logo_sprakradet_neg.png"
-        sizes="sm:100px md:120px lg:160px xl:180px"
+        width="180px"
         alt="logo språkrådet"
       />
-      <nuxt-img src="/logo_ugle_white.svg" sizes="lg:1vw" alt="logo uib" />
+      <nuxt-img src="/logo_ugle_white.svg" width="100px" alt="logo uib" />
       <div class="text-lg">
         <span>{{ $t("footer.contact") }}: </span><br/>
         <a href="mailto:termportalen@uib.no">termportalen@uib.no</a>
