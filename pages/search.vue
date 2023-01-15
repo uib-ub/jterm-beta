ae3153be58ccc65a554c3a0e37aa618d73f02743
Jan Ole Bangen <jan.bangen@uib.no>
2023-01-15 14:07:58 +0100

    add pred option to further

--- pages/search.vue
+++ pages/search.vue
@@ -141,6 +141,10 @@ const fetchFurtherSearchData = () => {
           searchFilterData.value.lang.length > 0
             ? searchFilterData.value.lang
             : searchOptions.value.searchLanguage,
+        searchPredicate:
+          searchFilterData.value.predicate.length > 0
+            ? searchFilterData.value.predicate
+            : searchOptions.value.searchPredicate,
         searchTranslate: searchOptions.value.searchTranslate,
         searchMatching: Object.keys(offset),
         searchLimit: searchOptions.value.searchLimit,
