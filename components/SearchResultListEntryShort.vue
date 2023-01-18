03c90df62fd350da363396d65294647540544ff3
Jan Ole Bangen <jan.bangen@uib.no>
2023-01-18 16:45:28 +0100

    feat: add component for search results list short

---
+++ components/SearchResultListEntryShort.vue
@@ -0,0 +1,30 @@
+<template>
+  <li class="rounded border">
+    <NuxtLink :to="`/${entryData.link}`">
+      <div class="p-2 hover:bg-gray-100 lg:flex">
+        <b
+          v-if="entryData.predicate == 'prefLabel'"
+          v-html="entryData.label"
+        ></b>
+        <span v-else v-html="entryData.label"></span>
+      </div>
+    </NuxtLink>
+  </li>
+</template>
+
+<script setup lang="ts">
+import { LangCode } from "../utils/vars";
+
+interface Props {
+  entryData: {
+    link: string;
+    predicate: string;
+    label: string;
+    lang: string[];
+    samling: string;
+    translate?: string;
+  };
+}
+
+const props = defineProps<Props>();
+</script>
