438d790fa98c2c0db2e919129839087786bcdaad
Jan Ole Bangen <jan.bangen@uib.no>
2023-01-20 11:46:11 +0100

    fix: domain menu spacing

--- components/DomainMenu.vue
+++ components/DomainMenu.vue
@@ -2,7 +2,7 @@
   <div class="px-1 text-sm md:text-base">
     <div class="rounded-t border border-b-0 border-gray-300">
       <button
-        class="min-w-full rounded-t p-1 text-left hover:bg-gray-100"
+        class="min-w-full rounded-t px-2 py-1 text-left hover:bg-gray-100"
         @click="displayDomainMenu = !displayDomainMenu"
       >
         <span v-if="searchOptions.searchDomain[0] !== 'all'">{{ $t("global.domain.domain") }}: </span>
@@ -20,7 +20,7 @@
           :class="{ activeDomain: searchOptions.searchDomain[0] == domain }"
         >
           <button
-            class="min-w-full p-1 text-left"
+            class="min-w-full px-2 py-1 text-left"
             :aria-current="searchOptions.searchDomain[0] == domain"
             @click="
               (searchOptions.searchDomain = [domain]),
