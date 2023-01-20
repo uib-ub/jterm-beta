ff0aa1d45c0c4f8d24b2c72763997aaac395de2a
Jan Ole Bangen <jan.bangen@uib.no>
2023-01-20 13:37:03 +0100

    feat: add option to provide langattr to data, move align inside comp

--- components/DataRow.vue
+++ components/DataRow.vue
@@ -1,23 +1,38 @@
 <template>
   <tr class="hover:bg-gray-100">
-    <th :class="thClass + ' pr-3 text-left font-semibold'" scope="row">{{ label }}</th>
-    <td v-if="to">
-      <NuxtLink v-if="nuxtlink" class="underline" :to="to">{{ data || to }}</NuxtLink>
+    <th :class="thClass + ' pr-3 text-left font-semibold'" scope="row">
+      {{ label }}
+    </th>
+    <td
+      v-if="to"
+      :lang="dataLang"
+      :class="{ 'text-right': langRtoL(dataLang as LangCode) }"
+    >
+      <NuxtLink v-if="nuxtlink" class="underline" :to="to">{{
+        data || to
+      }}</NuxtLink>
       <a v-else :href="to" class="underline">{{ data || to }}</a>
     </td>
-    <td v-else :class="{'text-right': dataRightAlign}">
+    <td
+      v-else
+      :class="{ 'text-right': langRtoL(dataLang as LangCode) }"
+      :lang="dataLang"
+    >
       {{ data }}
     </td>
   </tr>
 </template>
 
 <script setup lang="ts">
+import { LangCode } from "../utils/vars";
+
 const props = defineProps({
   data: { type: String, default: "" },
-  dataRightAlign: { type: Boolean, default: false },
+  dataRightAlign: { type: Boolean },
   thClass: { type: String, default: "" },
   label: { type: String, required: true },
   to: { type: String, default: "" },
   nuxtlink: Boolean,
+  dataLang: { type: String, default: null },
 });
 </script>
