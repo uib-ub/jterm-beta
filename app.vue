d8855472bc1104bea633b0f598417c367368ec79
Jan Ole Bangen <jan.bangen@uib.no>
2023-01-25 17:15:34 +0100

    refactor: move globally used class to app

--- app.vue
+++ app.vue
@@ -23,4 +23,14 @@ body {
 .tp-shighlight {
   @apply bg-tpblue-100;
 }
+
+.tp-search-dd {
+  margin-right: 7px;
+  margin-bottom: 7px;
+  background-color: white;
+  border: solid;
+  border-color: #d1d5db;
+  border-width: 1px;
+  border-radius: 4px;
+}
 </style>
--- components/SearchBarDropdown.vue
+++ components/SearchBarDropdown.vue
@@ -19,15 +19,3 @@ const props = defineProps({
   dropdown: { type: String, required: true },
 });
 </script>
-
-<style>
-.tp-search-dd {
-  margin-right: 7px;
-  margin-bottom: 7px;
-  background-color: white;
-  border: solid;
-  border-color: #d1d5db;
-  border-width: 1px;
-  border-radius: 4px;
-}
-</style>
