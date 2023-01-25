a62a706c41b47e8d89d09a403c28cd5e94c1c078
Jan Ole Bangen <jan.bangen@uib.no>
2023-01-25 12:16:49 +0100

    fix: add padding/h to domain menu

--- components/DomainMenu.vue
+++ components/DomainMenu.vue
@@ -2,7 +2,7 @@
   <div class="px-1 text-sm xs:text-base">
     <div class="rounded-t border border-b-0 border-gray-300">
       <button
-        class="min-w-full rounded-t px-2 py-1 text-left hover:bg-gray-100"
+        class="min-w-full rounded-t px-2 h-9 text-left hover:bg-gray-100"
         @click="displayDomainMenu = !displayDomainMenu"
       >
         <span v-if="searchOptions.searchDomain[0] !== 'all'">{{ $t("global.domain.domain") }}: </span>
@@ -20,7 +20,7 @@
           :class="{ activeDomain: searchOptions.searchDomain[0] == domain }"
         >
           <button
-            class="min-w-full px-2 py-1 text-left"
+            class="min-w-full px-2 py-2 text-left"
             :aria-current="searchOptions.searchDomain[0] == domain"
             @click="
               (searchOptions.searchDomain = [domain]),
