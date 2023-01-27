db4845772a80b54ebdee85c03a0e3ed8242a2544
Jan Ole Bangen <jan.bangen@uib.no>
2023-01-27 16:04:48 +0100

    feat: use icon in navbar and extend localization

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -11,17 +11,20 @@
       <div class="text-lg">
         <div class="xs:hidden">
           <button
-            class="ml-auto mr-0 flex rounded border border-white px-3 py-2 hover:border-gray-200 hover:text-gray-200"
+            class="ml-auto mr-0 flex rounded border border-white px-2 py-1 hover:border-gray-200 hover:text-gray-200"
+            :aria-label="
+              navMenuExpanded
+                ? $t('navBar.menuButtonHide')
+                : $t('navBar.menuButtonShow')
+            "
+            :title="
+              navMenuExpanded
+                ? $t('navBar.menuButtonHide')
+                : $t('navBar.menuButtonShow')
+            "
             @click="navMenuExpanded = !navMenuExpanded"
           >
-            <svg
-              class="h-3 w-3 fill-current"
-              viewBox="0 0 20 20"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <title>Menu</title>
-              <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
-            </svg>
+            <Icon name="tabler:menu-2" aria-hidden="true" size="1.2em" />
           </button>
         </div>
         <ul
