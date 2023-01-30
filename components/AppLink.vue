7e42c8376ea54d38cc07807702b1b7b88f96aa41
Jan Ole Bangen <jan.bangen@uib.no>
2023-01-30 12:26:24 +0100

    feat: add component for link handling
    fix: update cond handling in AppLink

---
+++ components/AppLink.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <NuxtLink v-if="isInternal === 'internal'" :to="to"><slot /></NuxtLink>
+    <NuxtLink v-if="isInternal === 'external'" :to="to"
+      ><span class="icon-pad">
+        <slot />
+      </span>
+      <Icon name="mdi:external-link" aria-hidden="true"
+    /></NuxtLink>
+    <NuxtLink v-else :to="to"><slot /></NuxtLink>
+  </div>
+</template>
+
+<script setup lang="ts">
+const props = defineProps({
+  to: { type: String, required: true },
+});
+const isInternal = computed(() => {
+  if (props.to.startsWith("/") || props.to.startsWith("#")) {
+    return "internal";
+  } else if (props.to.startsWith("http")) {
+    return "external";
+  }
+});
+</script>
+
+<style>
+.icon-pad {
+  padding-right: 2px;
+}
+</style>
