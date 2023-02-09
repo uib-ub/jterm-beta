4118727ef82dc263c2a2696e2493976c5b1977fd
Jan Ole Bangen <jan.bangen@uib.no>
2023-02-09 13:13:53 +0100

    feat: expand placeholder text and hide sb options on pages

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,11 +2,11 @@
   <div class="flex h-screen flex-col">
     <NavBar />
     <div
-      class="xs:p-4 container mx-auto max-w-screen-xl flex-1 bg-white p-2 sm:p-5 md:p-7 lg:p-9 xl:p-10"
+      class="xs:pb-4 xs:px-4 container mx-auto max-w-screen-xl flex-1 bg-white px-2 pb-2 sm:px-5 sm:pb-5 md:px-7 md:pb-7 lg:px-9 lg:pb-9 xl:px-10 xl:pb-10"
     >
       <h1
         v-if="route.path === '/'"
-        class="xs:text-6xl pb-6 text-5xl font-light tracking-tight text-black/75 antialiased sm:text-7xl md:text-8xl"
+        class="xs:text-6xl pt-[0.2em] text-5xl font-light tracking-tight text-black/75 antialiased sm:text-7xl md:text-8xl"
       >
         Termportalen
       </h1>
