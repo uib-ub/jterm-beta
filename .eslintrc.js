9f00d8ccdc106ed516465878f0df5ed4eb8e7d28
Jan Ole Bangen <jan.bangen@uib.no>
2023-03-13 14:30:01 +0100

    fix: add eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,5 +4,8 @@ module.exports = {
   rules: {
     "@typescript-eslint/no-unused-vars": ["off"],
     "tsdoc/syntax": "warn",
+    "import/default": "warn",
+    "react-hooks/exhaustive-deps": "off",
+    "react-hooks/rules-for-hooks": "off"
   },
 };
